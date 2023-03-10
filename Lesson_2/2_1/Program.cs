2cf1b082baee9814d593897412a6e6c2ebd58951
Светлана <skarpuhova@mail.ru>
2023-03-10 17:12:01 +0300

    Task 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int result1=num % 10;
+    int result2=num % 10;
+    if (result1>result2)
+    {
+        return result1;
+    }
+    else
+
+    {
+        return result2;
+
+    }
+}
+ int itog=TakeNum(new Random().Next(10,100));
+ Console.WriteLine(itog);
