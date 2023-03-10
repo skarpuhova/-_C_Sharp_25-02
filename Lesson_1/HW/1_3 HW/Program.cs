4371df567bcf542e11e143275c388a6275746fbe
Светлана <skarpuhova@mail.ru>
2023-03-10 14:38:53 +0300

    1_3 HW

---
+++ Lesson_1/HW/1_3 HW/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите целое число больше 1:");
+int inputNum = int.Parse(Console.ReadLine()!);
+int count = 2;
+while(count <= inputNum)
+{
+Console.Write($"{count}");
+count = count + 2;
+if (count <= inputNum)
+Console.Write(", ");
+}
