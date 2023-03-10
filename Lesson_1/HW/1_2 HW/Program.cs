65ffedf62c9a51543c23d6b9898e3fedef5b6fb8
Светлана <skarpuhova@mail.ru>
2023-03-10 14:23:39 +0300

    1_2 HW

---
+++ Lesson_1/HW/1_2 HW/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max = 0;
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+ Console.WriteLine(max);
