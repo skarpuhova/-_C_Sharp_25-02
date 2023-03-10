19a5ba4ad56de7706b2e099c6aef71f678d3d2e2
Светлана <skarpuhova@mail.ru>
2023-03-10 14:09:29 +0300

    1_1 HW

---
+++ Lesson_1/HW/1_1 HW/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string s_n_1=Console.ReadLine()!;
+string s_n_2=Console.ReadLine()!;
+int n_1=int.Parse(s_n_1);
+int n_2=int.Parse(s_n_1);
+if(n_1>n_2)
+{
+    Console.WriteLine($"The number{n_1} is greater than{n_2}");
+}
+else if(n_1==n_2)
+{
+    Console.Write($"The number{n_1} is equal to the number{n_2}");
+}
+else
+{
+    Console.Write($"The number{n_2} is greater than{n_1}");
+}
