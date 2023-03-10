d0664e12fc33a527096e6c1863f3e45c01cd52d6
Светлана <skarpuhova@mail.ru>
2023-03-10 14:49:19 +0300

    1_4 HW

---
+++ Lesson_1/HW/1_4 HW/Program.cs
@@ -0,0 +1,10 @@
+﻿
+void Find(int num)
+{
+if (num % 2 == 0)
+Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+}
+
+Console.Write("Введите число для проверки на четность:");
+Find(int.Parse(Console.ReadLine()!));
