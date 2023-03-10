b0896f8151413ce78e6103ba698b6084f60dee54
Светлана <skarpuhova@mail.ru>
2023-03-10 20:34:32 +0300

    Task 2_1

---
+++ Lesson_2/HW/1_1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string NumSec(int num)
+{
+    if ((-1000 < num && num > 100) || (num < 1000 && num > 100))
+        return $"{num / 10 % 10}";
+        return "The number is not three - digit!";
+}
+Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
