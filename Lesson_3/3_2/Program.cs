2c394d12e1983917a28c9045da0ab75bbb67b917
Светлана <skarpuhova@mail.ru>
2023-03-14 22:43:58 +0300

    Task 3_2

---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,6 @@
+﻿
+double example(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Sqrt(Math.Pow(y2-y1, 2)));
+}
+Console.WriteLine(example(3, 6, 2, 1));
