aa7ca458fda069b452fa56c65bca569b5e70cb2b
Светлана <skarpuhova@mail.ru>
2023-03-15 22:12:13 +0300

    task 3_1

--- Lesson_3/HW/3_1/Program.cs
+++ Lesson_3/HW/3_1/Program.cs
@@ -1,16 +1,17 @@
 ﻿
 string Palindrome(int n)
 {
-    int num=10000;
-    if (n > 100000 || n < 10000)
-      return "The number is not five-digit!";
+   int num=10000;
+      if (n > 100000 || n < 10000)
+         return "The number is not five-digit!";
     while (num > 1)
     {
-         if (n / num % 10  ! = n % 10)
-            return "no";
-         n/=10;
-         num/=100;
+    
+    if (n/num % 10  != n % 10)
+               return "no";
+            n /= 10;
+            num /= 100;
     }
-       return "yes";
+         return "yes";
 }
-Console.WriteLine(Palindrome(int.Parse(Console.ReadLine()!)));
+     Console.WriteLine(Palindrome(int.Parse(Console.ReadLine()!)));
