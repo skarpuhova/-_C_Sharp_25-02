27635249e43c6c66cf9706b0edcc701b6cd48a54
Светлана <skarpuhova@mail.ru>
2023-03-15 14:00:46 +0300

    task 3_2

---
+++ Lesson_3/HW/3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+double Line3D(double x1,double y1,
+double z1,double x2,double y2,double z2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2)
+    + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2)), 2);
+}
+double x_1=double.Parse(Console.ReadLine()!);
+double y_1=double.Parse(Console.ReadLine()!);
+double z_1=double.Parse(Console.ReadLine()!);
+double x_2=double.Parse(Console.ReadLine()!);
+double y_2=double.Parse(Console.ReadLine()!);
+double z_2=double.Parse(Console.ReadLine()!);
+Console.WriteLine(Line3D(x_1,y_1,z_1,x_2,y_2,z_2));
