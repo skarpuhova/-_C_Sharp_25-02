5fff356284761835b00a263ba41cf70969d7d953
Светлана <skarpuhova@mail.ru>
2023-03-15 14:19:57 +0300

    task 3_3

---
+++ Lesson_3/HW/3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void Squares(int n)
+{
+    double[] nums=new double[n];
+    int index=0;
+    Console.Write($"{n} - >");
+    while(index<n)
+    {
+        nums[index] = Math.Pow((index+1), 3);
+        Console.Write($"{nums[index]},");
+        index += 1;
+    }
+    Console.WriteLine();
+}
+Squares(int.Parse(Console.ReadLine()!));
