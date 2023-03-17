82fe5a71074e612846872a59c4d8c64b32a9913d
Светлана <skarpuhova@mail.ru>
2023-03-17 12:14:42 +0300

    task 4_0

---
+++ Lesson_4/4_0/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int SumNum(int num)
+{
+    int all_sum=0;
+    for(int i=1; i<=num;i++)
+        all_sum +=i;
+    return all_sum;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
