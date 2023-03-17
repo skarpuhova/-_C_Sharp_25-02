d6bfe9990c7b0669f3fdb87fe52bb15b9bbae531
Светлана <skarpuhova@mail.ru>
2023-03-17 20:08:44 +0300

    task 4_1 hw

---
+++ Lesson_4/HW/4_1 HW/Program.cs
@@ -0,0 +1,20 @@
+﻿
+double PowNum(int a,int b)
+{
+    double n_pow=1;
+    int b_abs=Math.Abs(b);
+
+    for (int i=1; i<=b_abs; i++)
+{
+    if(b>0)
+       n_pow*=a;
+    else
+       n_pow/=a;
+}
+return n_pow;
+}
+Console.WriteLine("Enter a number: ");
+int num_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter degree of number: ");
+int num_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PowNum(num_1,num_2));
