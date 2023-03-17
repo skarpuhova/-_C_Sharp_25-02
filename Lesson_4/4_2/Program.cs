f650d4ce3d249da8acff3d86e4dc13d0ac4d5174
Светлана <skarpuhova@mail.ru>
2023-03-17 15:33:44 +0300

    task 4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int ProNum(int num)
+{
+    int kol = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        kol = kol * i;
+    }
+    return kol;
+}
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(ProNum(A));
