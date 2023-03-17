c601c3e1b5a4b84369d9b90e4cf350183c67ea41
Светлана <skarpuhova@mail.ru>
2023-03-17 19:08:46 +0300

    task 4_3

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+void Massiv(int leght)
+{
+    int[] R = new int[leght];
+    for(int i=0; i<leght;i++)
+    {
+        R[i] = new Random().Next(2);
+        Console.WriteLine(R[i]);
+    }
+}
+Massiv(int.Parse(Console.ReadLine()!));
