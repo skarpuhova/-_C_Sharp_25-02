7fb887494ca140ebb24ff2db1bafbab8b4dcdafe
Светлана <skarpuhova@mail.ru>
2023-03-21 13:56:37 +0300

    task 5_0

---
+++ Lesson_5/5_0/Program.cs
@@ -0,0 +1,37 @@
+﻿
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}");
+    Console.WriteLine();
+}
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+void SumPosNeg(int[] arr)
+{
+    int pos, neg;
+    pos = neg = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            pos += arr[i];
+        else
+            neg += arr[i];
+    }
+    Console.WriteLine($"Positive:{pos}, negative:{neg}");
+}
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int []mass = MassNums(num, start, stop);
+Print(mass);
+SumPosNeg(mass);
