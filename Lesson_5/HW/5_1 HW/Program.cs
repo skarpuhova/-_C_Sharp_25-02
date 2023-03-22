34a22593dc972ad3e370c2c241ca57410f061849
Светлана <skarpuhova@mail.ru>
2023-03-22 12:37:38 +0300

    task 5_1 hw

---
+++ Lesson_5/HW/5_1 HW/Program.cs
@@ -0,0 +1,34 @@
+﻿
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]}");
+    Console.WriteLine();
+}
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+int EvenNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]) % 2==0
+        count += 1;
+    }
+    return count;
+}
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Console.WriteLine(EvenNum(mass));
