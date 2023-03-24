506ba70c85de0f981ab72825080d2b75c510f24f
Светлана <skarpuhova@mail.ru>
2023-03-24 12:39:21 +0300

    task 6_0

---
+++ Lesson_6/6_0/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void Trngl(int A, int B, int C)
+{
+    if(A<B+C && B<A+C && C<A+B)
+    Console.WriteLine($"Да, такой треугольник может быть");
+    else Console.WriteLine($"Нет, такого треугольника не может быть");
+    
+}
+int num_1=int.Parse(Console.ReadLine()!);
+int num_2=int.Parse(Console.ReadLine()!);
+int num_3=int.Parse(Console.ReadLine()!);
+
+Trngl(num_1,num_2,num_3);
+
+
