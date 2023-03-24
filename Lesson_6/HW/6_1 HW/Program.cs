a435416e2f2959af5c985b939b64eb4c4494635e
Светлана <skarpuhova@mail.ru>
2023-03-24 16:34:00 +0300

    task 6_1 hw

---
+++ Lesson_6/HW/6_1 HW/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int Check(int num)
+{
+    int count=0;
+    for(int i=1; i<=num; i++ )
+    {
+        Console.Write($"Число {i}:");
+        int a=int.Parse(Console.ReadLine()!);
+
+        if(a>0) count++;
+    }
+    return count;
+        
+}
+Console.Write("Сколько чисел хотели бы ввести?:");
+int a=int.Parse(Console.ReadLine()!);
+int result=Check(a);
+Console.WriteLine($"Количество чисел больше 0:{result}");
