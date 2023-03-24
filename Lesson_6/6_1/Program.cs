31e36f99baf8e06339d15874f5dc6ef5e774ec27
Светлана <skarpuhova@mail.ru>
2023-03-24 13:22:19 +0300

    task 6_1

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string convert(int num_1)
+{
+    string result="";
+    while(num_1>0)
+    {
+        result=num_1 % 2 + result;
+        num_1=num_1/2;
+    }
+    return result;
+}
+Console.WriteLine(convert(12));
